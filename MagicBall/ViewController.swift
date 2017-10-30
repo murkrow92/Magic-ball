0cf238e6e7fc41fe66e66ed2dbd67146f27ccb7e
Duc Trung <ducdt1992@gmail.com>
2017-10-30 10:19:22 +0700

    + update view controller

--- MagicBall/ViewController.swift
+++ MagicBall/ViewController.swift
@@ -1,24 +1,14 @@
-//
-//  ViewController.swift
-//  MagicBall
-//
-//  Created by murkrow on 10/18/17.
-//  Copyright © 2017 Murkrow. All rights reserved.
-//
-
 import UIKit
-
 
 class ViewController: UIViewController {
 
     private let ARRAY_IMAGES = ["ball1", "ball2", "ball3", "ball4", "ball5"]
-    var randomDiceNumber: Int = 0;
+    private var randomDiceNumber: Int = 0;
 
     @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         let index = Int(arc4random_uniform(4))
         imageView.image = UIImage(named: ARRAY_IMAGES[index])
     }
@@ -26,7 +16,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func askButtonPressed(_ sender: UIButton) {
